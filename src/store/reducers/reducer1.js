b27145acb6e33ad8cd60f114be49b4ab7572261a
Cai <trieungochai.dev@gmail.com>
2020-09-20 16:13:55 +0700

    Setting up the actions & reducers

---
+++ src/store/reducers/reducer1.js
@@ -0,0 +1,24 @@
+import * as ACTION_TYPES from '../actions/action_types';
+
+const initialState = {
+  stateProp1: false,
+};
+
+const Reducer1 = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SUCCESS:
+      return {
+        ...state,
+        stateProp1: true,
+      };
+    case ACTION_TYPES.FAILURE:
+      return {
+        ...state,
+        stateProp1: false,
+      };
+    default: return state;
+  };
+};
+
+
+export default Reducer1;
