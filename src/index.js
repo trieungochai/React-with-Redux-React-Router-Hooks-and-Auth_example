9945ec8e5cd8ee791905ba8b602700653fbe80c1
Cai <trieungochai.dev@gmail.com>
2020-09-20 16:19:22 +0700

    createStore()

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducer1 from './store/reducers/reducer1';
+
+let store = createStore(Reducer1);
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
